de2ef5785188e20e42f0ba562a9f7ed7a018eba5
plainphw <s2028@e-mirim.hs.kr>
2022-06-29 09:38:48 +0900

    #1 메시지박스, 모달, 모달창, MDI 폼 작성

--- ModalModeless/Form1.cs
+++ ModalModeless/Form1.cs
@@ -15,6 +15,47 @@ namespace ModalModeless
         public Form1()
         {
             InitializeComponent();
+            IsMdiContainer = true;
+            // 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요.", "장마철 입니다.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요.", "장마철 입니다.", MessageBoxButtons.YesNoCancel);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("안녕하세요.", "장마철 입니다.", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm();
+            // frm.MdiParent = this;
+            // 모달창은 사용 불가
+            frm.ShowDialog();
+            // Modal 창을 띄울 때는 ShowDialog()
+            // 해당 창에만 커서 동작 가능 -> 본체는 해당 창을 없앤 후 가능
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CustomForm frm = new CustomForm();
+            frm.MdiParent = this;
+            frm.Show();
+            // Modeless 창을 띄울 때는 Show()
+            // 해당 창 외에도 커서 동작 가능
         }
     }
 }
